ff78e2bd3bd3382ba5297a400889cbc540552204
gpekd5 <gpekd5@naver.com>
2026-04-13 20:31:51 +0900

    Feat: 전체 일정 조회 API 추가

--- src/main/java/com/example/scheduleproject/controller/ScheduleController.java
+++ src/main/java/com/example/scheduleproject/controller/ScheduleController.java
@@ -2,13 +2,14 @@ package com.example.scheduleproject.controller;
 
 import com.example.scheduleproject.dto.CreateScheduleRequest;
 import com.example.scheduleproject.dto.CreateScheduleResponse;
+import com.example.scheduleproject.dto.GetScheduleResponse;
 import com.example.scheduleproject.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,5 +20,10 @@ public class ScheduleController {
     @PostMapping("/schedules")
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.save(request));
+    }
+
+    @GetMapping("/schedules")
+    public ResponseEntity<List<GetScheduleResponse>> getAllSchedule(@RequestParam(required = false) String writer) {
+        return  ResponseEntity.status(HttpStatus.OK).body(scheduleService.findAll(writer));
     }
 }
--- src/main/java/com/example/scheduleproject/repository/ScheduleRepository.java
+++ src/main/java/com/example/scheduleproject/repository/ScheduleRepository.java
@@ -1,7 +1,11 @@
 package com.example.scheduleproject.repository;
 
 import com.example.scheduleproject.entity.Schedule;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByWriter(String writer, Sort sort);
 }
